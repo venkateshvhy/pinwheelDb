51541c64a3dd6cb9ea3ed81f499a0c43da385962
venkateshvhy <130738415+venkateshvhy@users.noreply.github.com>
2025-01-15 18:01:05 +0530

    Delete dbo directory

--- dbo/StoredProcedures/FooterMenus.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[FooterMenus]
-AS
-BEGIN
-Set NoCount On
-select * from Menu m
-where m.IsFooter = 1
-order by m.FooterOrder asc
-END;
--- dbo/StoredProcedures/ListMenus.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[ListMenus]
-	As
-	BEGIN
-	Set NoCount On
-	select * from Menu m
-	where m.Type = 'header';
-	END;
